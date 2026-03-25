fd5b359f30d969e99bbc70560f865fd2b9bcced6
Vinicius G. Cardoso <vini.vgc@gmail.com>
2026-03-25 03:07:14 -0700

    change app to test ci

--- apps/sample-app/app.py
+++ apps/sample-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Platform Engineering Project 2 🚀"
+    return "Hello from Platform Engineering Project 🚀"
 
 @app.route("/health")
 def health():
