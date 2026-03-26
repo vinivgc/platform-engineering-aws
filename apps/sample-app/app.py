d93529abf75d76cb02e08f97dacc75db9b8087c0
Vinicius G. Cardoso <vini.vgc@gmail.com>
2026-03-26 02:23:28 -0700

    update app to create image for new infra

--- apps/sample-app/app.py
+++ apps/sample-app/app.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Platform Engineering Project 2 🚀"
+    return "Hello from Platform Engineering Project 🚀"
 
 @app.route("/health")
 def health():
     return {"status": "ok"}, 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
