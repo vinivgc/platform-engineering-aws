2a15cdde119b670dbd58ebfc7fd9c06752b1486e
Vinicius G. Cardoso <vini.vgc@gmail.com>
2026-03-27 07:16:50 -0700

    change app to test once more the cd

--- apps/sample-app/app.py
+++ apps/sample-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Platform Engineering Project 3 🚀"
+    return "Hello from Platform Engineering Project 4 🚀"
 
 @app.route("/health")
 def health():
